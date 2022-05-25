648e62b017d744f550a113ded393d7bfd761f163
chrystyan allyson <chrystyan.allyson@gmail.com>
2022-05-25 11:26:50 -0300

    atualização

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 var dados = [];
+function populatabela(){
+    if(Array.isArray(dados)){
+        $("#tblDados tbody").html("")
+        dados.forEach(function(item){
+            $("tblDados tbody").append(`<tr>
+                <td>${item.ID}</td>
+                <td>${item.Nome}</td>
+                <td>${item.Sobrenome}</td>
+                <td>${item.DtNascimento}</td>
+                <td>${item.Formação}</td>
+            </tr>`)
+        })
+    }
+}
+
 $(function (){
-    
+    dados = JSON.parse(localStorage.getItem("__dados__"))
+    if(dados){
+        populatabela()
+    }
+    $("#btnSalvar").click(function(){
+        
+    })
 
 })
